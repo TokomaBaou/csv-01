d430da4b55131f1ad085236c6ed07880c3010fd9
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-09 22:48:09 +0900

    csv設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,59 @@
 import "./styles.css";
+import ExcelJS from "exceljs";
+import React, { FC } from "react";
 
-export default function App() {
+export const App: FC = () => {
+  const handlerClickDownloadButton = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    format: "xlsx" | "csv"
+  ) => {
+    e.preventDefault();
+
+    const workbook = new ExcelJS.Workbook();
+    workbook.addWorksheet("sheet1");
+    const worksheet = workbook.getWorksheet("sheet1");
+    worksheet.columns = [
+      { header: "ID", key: "id" },
+      { header: "作成日時", key: "createAt" },
+      { header: "名前", key: "name" }
+    ];
+
+    worksheet.addRows([
+      {
+        id: "f001",
+        createAt: 1629902208,
+        name: "りんご"
+      },
+      {
+        id: "f002",
+        createAt: 1629902245,
+        name: "ぶどう"
+      },
+      {
+        id: "f003",
+        createAt: 1629902265,
+        name: "バナナ"
+      }
+    ]);
+
+    const unit8Array =
+      format === "xlsx"
+        ? await workbook.xlsx.writeBuffer()
+        : await workbook.csv.writeBuffer();
+
+    const blob = new Blob([unit8Array], { type: "application/octet-binary" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "sampleData." + format;
+    a.click();
+    a.remove();
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>データ出力</h1>
+      <button>CSV形式</button>
     </div>
   );
-}
+};
